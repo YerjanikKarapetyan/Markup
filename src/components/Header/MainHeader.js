90b1af409705eb60e2933ca5c56cddd5b17808a6
Yerjanik <yerjanikkarapetyan@gmail.com>
2021-11-13 20:57:07 +0400

    initial commit

---
+++ src/components/Header/MainHeader.js
@@ -0,0 +1,33 @@
+import React  from 'react';
+import Main from '../Main/Main';
+import Other from './Other/Other'
+import Pictures from './Pictures/Pictures';
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+import './MainHeader.css';
+
+const MainHeader = () =>{
+
+return(
+      <Router>
+      <div className="MainHeader">
+          <ul className='MainHeader-ul'>
+            <li>
+              <Link to="/Main">Main</Link>
+            </li>
+            <li>
+              <Link to="/Pictures">Pictures</Link>
+            </li>
+            <li>
+              <Link to="/Other">Other</Link>
+            </li>
+          </ul>
+      </div>
+      <Routes>
+          <Route path="/Main" element={<Main/>} />
+          <Route path="/Pictures" element={<Pictures/>}/>
+          <Route path="/Other" element={<Other/>}/>
+        </Routes>
+    </Router>
+    );
+}
+export default MainHeader;
---
+++ src/components/Header/Pictures/Pictures.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Pictures = () =>{
+    
+    return(
+        <div>
+            Pictures
+        </div>
+    )
+}
+
+export default Pictures;
